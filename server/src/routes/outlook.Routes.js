daaf2c402478bf566bbfd4af55bd21745d8d107b
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-03-31 21:12:07 +0530

    prompt updated

--- server/src/routes/outlook.Routes.js
+++ server/src/routes/outlook.Routes.js
@@ -2,12 +2,9 @@ const express = require('express');
 // const app = express();
 const outlookRouter = express.Router();
 const {
-    // handleAuthorization,
-    // getMailsFromOutlook,
     signin,
     callback,
     getAccessToken
-    // getAccessTokenFromOutlook
 } = require('../controllers/outlookController');
 
 outlookRouter.use(express.json());
@@ -17,7 +14,6 @@ outlookRouter.use(express.urlencoded({ extended: true }));
 outlookRouter.get('/signin', signin);
 outlookRouter.get('/callback', callback);
 outlookRouter.get('/get-access-token', getAccessToken);
-// outlookRouter.get('/get-access-token', getAccessTokenFromOutlook);
 // outlookRouter.get('/get-mails/:num', getMailsFromOutlook);
 
 module.exports = outlookRouter;
