d3387fadd2edd8a56830a06a888ab81e53649d35
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-04-01 14:38:23 +0530

    comments added

--- server/src/controllers/outlookController.js
+++ server/src/controllers/outlookController.js
@@ -1,23 +1,18 @@
 const express = require("express");
-// const app = express();
-// const { Client } = require("@microsoft/microsoft-graph-client");
 require("dotenv").config();
 const { createConfig } = require("../helpers/utils");
 const axios = require("axios");
-const {
-  connection,
-  redisGetToken,
-} = require("../middlewares/redis.middleware");
+const { connection, redisGetToken, } = require("../middlewares/redis.middleware");
 const { ConfidentialClientApplication } = require("@azure/msal-node");
-
 const OpenAI = require("openai");
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_SECRECT_KEY });
 
+
 const clientId = process.env.AZURE_CLIENT_ID;
 const clientSecret = process.env.AZURE_CLIENT_SECRET;
 // const redirectUri = "http://localhost:4400/outlook/callback";
-const redirectUri = "https://reachinbox-assignment-4rf9.onrender.com/outlook/callback";
+const redirectUri ="https://reachinbox-assignment-4rf9.onrender.com/outlook/callback";
 const scopes = ["user.read", "Mail.Read", "Mail.Send"];
 
 const ccaConfig = {
@@ -30,6 +25,7 @@ const ccaConfig = {
 
 const cca = new ConfidentialClientApplication(ccaConfig);
 
+// outlook signin 
 const signin = (req, res) => {
   const authCodeUrlParameters = {
     scopes,
@@ -43,7 +39,7 @@ const signin = (req, res) => {
 
 
 let accessToken;
-
+// outlook callback
 const callback = async (req, res) => {
   const { code } = req.query;
 
@@ -66,7 +62,7 @@ const callback = async (req, res) => {
 
     accessToken = response.accessToken;
     console.log(accessToken);
-    res.status(200).send({msg:"user is authorized"});
+    res.status(200).send({ msg: "user is authorized" });
   } catch (error) {
     console.error("Error exchanging authorization code:", error.message);
     console.log(error);
@@ -74,6 +70,8 @@ const callback = async (req, res) => {
   }
 };
 
+
+// get user profile
 const getUser = async (req, res) => {
   try {
     const url = `https://graph.microsoft.com/v1.0/me`;
@@ -98,6 +96,7 @@ const getUser = async (req, res) => {
   }
 };
 
+// get all emails from authorized user
 const getMails = async (req, res) => {
   try {
     const url = `https://graph.microsoft.com/v1.0/me/messages?maxResults=50`;
@@ -117,6 +116,7 @@ const getMails = async (req, res) => {
   }
 };
 
+// read particular mail from outlook
 const readMail = async (req, res) => {
   try {
     const url = `https://graph.microsoft.com/v1.0/me/messages/${req.params.message}`;
@@ -134,6 +134,8 @@ const readMail = async (req, res) => {
   }
 };
 
+
+// send mail using outlook
 const sendMail = async (data, token) => {
   try {
     if (!token) {
@@ -145,12 +147,12 @@ const sendMail = async (data, token) => {
     if (data.label === "Interested") {
       emailContent = `If the email mentions they are interested, do not generate any recipient's name instead use Dear user, your reply should give this advertisement i have express some key points below user then and create good reply for advertisement it should render on email in bullet points
       
-We're excited to share with you how our product can benefit you:
-- Secure Mailing: Our platform offers end-to-end encryption to ensure your emails remain private and secure.
-- Automated Emails: Easily automate your email workflows by setting timers and triggers. Schedule emails to be sent at specific times or based on user actions.
-- Customizable Templates: Create personalized email templates and automate repetitive tasks, saving you time and effort.
+      We're excited to share with you how our product can benefit you:
+      - Secure Mailing: Our platform offers end-to-end encryption to ensure your emails remain private and secure.
+      - Automated Emails: Easily automate your email workflows by setting timers and triggers. Schedule emails to be sent at specific times or based on user actions.
+      - Customizable Templates: Create personalized email templates and automate repetitive tasks, saving you time and effort.
 
-Would you like to learn more about how our platform can streamline your email communication? Feel free to reply to this email.`;
+      Would you like to learn more about how our platform can streamline your email communication? Feel free to reply to this email.`;
       subject = `User is : ${data.label}`;
     } else if (data.label === "Not Interested") {
       emailContent = `If the email mentions they are not interested, create a reply where we should ask them for feedback on why they are not interested. do not generate any recipient's name instead use Dear user. Write a small text on the above request in around 100-150 words`;
@@ -158,13 +160,13 @@ Would you like to learn more about how our platform can streamline your email co
     } else if (data.label === "More Information") {
       emailContent = `If the email mentions they are interested to know more, your reply should give them more information about this product. Here are some of its key features:
       
-Thank you for expressing interest in our product! We're thrilled to share more details with you:
-- Google Authentication: Allow users to authenticate using their Google accounts.
-- View User Profile: Retrieve and display user profile information such as name, email, and profile picture.
-- View All Drafts: Fetch and display a list of all draft emails associated with the user's email address.
-- Read Specific Email: Retrieve and display the content of a specific email using its ID.
-- List Mails: Fetch and display a list of all emails associated with the user's email address.
-- Send Email with Label: Allow users to send emails with a specified label (e.g., "Interested", "Not Interested", "More Information").`;
+      Thank you for expressing interest in our product! We're thrilled to share more details with you:
+      - Google Authentication: Allow users to authenticate using their Google accounts.
+      - View User Profile: Retrieve and display user profile information such as name, email, and profile picture.
+      - View All Drafts: Fetch and display a list of all draft emails associated with the user's email address.
+      - Read Specific Email: Retrieve and display the content of a specific email using its ID.
+      - List Mails: Fetch and display a list of all emails associated with the user's email address.
+      - Send Email with Label: Allow users to send emails with a specified label (e.g., "Interested", "Not Interested", "More Information").`;
       subject = `User is : ${data.label}`;
     }
 
@@ -183,14 +185,8 @@ Thank you for expressing interest in our product! We're thrilled to share more d
     const [heading, features, benefits] =
       response.choices[0].message.content.split("\n\n");
     const headingHTML = `<h2>${heading}</h2>`;
-    const featuresHTML = `<ul>${features
-      .split("\n")
-      .map((feature) => `<li>${feature}</li>`)
-      .join("")}</ul>`;
-    const benefitsHTML = `<ul>${benefits
-      .split("\n")
-      .map((feature) => `<li>${feature}</li>`)
-      .join("")}</ul>`;
+    const featuresHTML = `<ul>${features.split("\n").map((feature) => `<li>${feature}</li>`).join("")}</ul>`;
+    const benefitsHTML = `<ul>${benefits.split("\n").map((feature) => `<li>${feature}</li>`).join("")}</ul>`;
 
     const mailOptions = {
       message: {
@@ -227,9 +223,6 @@ Thank you for expressing interest in our product! We're thrilled to share more d
     throw new Error("Can't send email: " + error.message);
   }
 };
-
-
-
 
 module.exports = {
   getUser,
